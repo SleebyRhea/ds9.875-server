ea2ae55a08a150c7dd761f861978e162b0ff4ca6
Andrew Wyatt <arcturus615@gmail.com>
2019-11-27 11:19:15 +0000

    Remove unintentional creative mode check

--- root/srv/avorion/mods/ds9utils-allcolors/data/scripts/player/init.lua
+++ root/srv/avorion/mods/ds9utils-allcolors/data/scripts/player/init.lua
@@ -3,9 +3,7 @@ if onServer() then
 
 local player = Player()
 	print("[AllOfTheColors] Adding all colors to " .. player.name)
-    if player.infiniteResources then
-        for _, color in pairs({ColorPalette()}) do
-            player:addColor(color)
-        end
-    end
+	for _, color in pairs({ColorPalette()}) do
+		player:addColor(color)
+	end
 end
