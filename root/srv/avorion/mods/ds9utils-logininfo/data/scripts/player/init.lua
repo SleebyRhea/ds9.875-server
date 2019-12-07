61efec216d86904472bb76665ebb8f1c3231a6ea
Andrew Wyatt <arcturus615@gmail.com>
2019-12-07 13:43:13 +0000

    Add ds9utils-logininfo

---
+++ root/srv/avorion/mods/ds9utils-logininfo/data/scripts/player/init.lua
@@ -0,0 +1,36 @@
+if onServer() then
+	local player = Player()
+	local old_path = package.path
+	package.path = package.path .. ";data/scripts/lib/?.lua"
+	
+	local __d = {
+		mod = 'ds9utils-logininfo',
+		plr = player.name,
+		coords = Sector().name,
+		name_craft = (player.craft and player.craft.name or "None (Flying In Drone)"),
+		name_alliance = (player.alliance and player.alliance.name or "None"),
+		count_ships = (player.numShips and player.numShips or 0),
+		count_stations = (player.numShips and player.numShips or 0),
+	}
+
+    if player.craft and player.craft.type ~= EntityType.Drone then
+		__d["count_blocks"] = player:getShipPlan(player.craft.name).numBlocks
+	else
+		__d["count_blocks"] = 1
+	end
+
+	-- While I would LOVE nothing more than to just the fancy table up above, the string
+	-- utility does NOT make use of a raw Lua table when passed it. Thus, the following
+	-- syntax MUST be used. I've tried to make this as painless to interpret as possible.
+	include ("stringutility")
+	print("")
+	print("[${m}] Printing Statistics for <${p}>"%_T % {m=__d.mod,p=__d.plr} )
+	print("[${m}] ${p} ::> Current Location: <${n}>"%_T % {m=__d.mod,p=__d.plr,n=__d.coords} )
+	print("[${m}] ${p} ::> Current Ship Name: <${n}>"%_T % {m=__d.mod,p=__d.plr,n=__d.name_craft} )
+	print("[${m}] ${p} ::> Current Ship Block Count: <${n2}>"%_T % {m=__d.mod,p=__d.plr,n=__d.name_craft,n2=__d.count_blocks} )
+	print("[${m}] ${p} ::> Total Station Count: <${n}>"%_T % {m=__d.mod,p=__d.plr,n=__d.count_stations} )
+	print("[${m}] ${p} ::> Total Ship Count: <${n}>"%_T % {m=__d.mod,p=__d.plr,n=__d.count_ships} )
+	print(" ")
+
+	package.path = old_path
+end
---
+++ root/srv/avorion/mods/ds9utils-logininfo/modinfo.lua
@@ -0,0 +1,14 @@
+meta =
+{
+    id =               "ds9utils-logininfo",
+    name =             "ds9utils-logininfo",
+    title =            "DS9 Utilities: Playerinfo on Login",
+    description =      "Vomit player data to the primary logfile when they log in",
+    authors =          {"ds9.875-team"},
+    version =          "1.0",
+    dependencies =     {{id = "Avorion", min = "0.22"}},
+    serverSideOnly =   true,
+    clientSideOnly =   false,
+    saveGameAltering = true,
+    contact =          "@Cyberpunker#3611",
+}
