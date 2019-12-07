64ceb305d4c896f11f18306b7d98ac4407577bcd
Andrew Wyatt <arcturus615@gmail.com>
2019-12-07 14:06:04 +0000

    Update moddate across mods

--- root/srv/avorion/mods/ds9utils-welcomemail/modinfo.lua
+++ root/srv/avorion/mods/ds9utils-welcomemail/modinfo.lua
@@ -1,41 +1,14 @@
 meta =
 {
-    id = "ds9875WelcomeEmail",
-    name = "ds9875WelcomeEmail",
-    title = "DS9.875 Utils: Welcome Email",
-    description = "Adds a sends a welcome email with a turret and some resources to players on creation",
-    authors = {"Arcturus615","Darenkal (Conico)"},
-    version = "0.1",
-
-    -- If your mod requires dependencies, enter them here. The game will check that all dependencies given here are met.
-    -- Possible attributes:
-    -- id: The ID of the other mod as stated in its modinfo.lua
-    -- min, max, exact: version strings that will determine minimum, maximum or exact version required (exact is only syntactic sugar for min == max)
-    -- optional: set to true if this mod is only an optional dependency (will only influence load order, not requirement checks)
-    -- incompatible: set to true if your mod is incompatible with the other one
-    -- Example:
-    -- dependencies = {
-    --      {id = "Avorion", min = "0.17", max = "0.21"}, -- we can only work with Avorion between versions 0.17 and 0.21
-    --      {id = "SomeModLoader", min = "1.0", max = "2.0"}, -- we require SomeModLoader, and we need its version to be between 1.0 and 2.0
-    --      {id = "AnotherMod", max = "2.0"}, -- we require AnotherMod, and we need its version to be 2.0 or lower
-    --      {id = "IncompatibleMod", incompatible = true}, -- we're incompatible with IncompatibleMod, regardless of its version
-    --      {id = "IncompatibleModB", exact = "2.0", incompatible = true}, -- we're incompatible with IncompatibleModB, but only exactly version 2.0
-    --      {id = "OptionalMod", min = "0.2", optional = true}, -- we support OptionalMod optionally, starting at version 0.2
-    -- },
-    dependencies = {
-        {id = "Avorion", min = "0.23"}
-    },
-
-    -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
-    serverSideOnly = true,
-
-    -- Set to true if the mod only has to run on the client, such as UI mods
-    clientSideOnly = false,
-
-    -- Set to true if the mod changes the savegame in a potentially breaking way, as in it adds scripts or mechanics that get saved into database and no longer work once the mod gets disabled
-    -- logically, if a mod is client-side only, it can't alter savegames, but Avorion doesn't check for that at the moment
+    id =               "ds9875WelcomeEmail",
+    name =             "ds9875WelcomeEmail",
+    title =            "DS9.875 Utils: Welcome Email",
+    description =      "Adds a sends a welcome email with a turret and some resources to players on creation",
+    authors =          {"ds9.875-team","Darenkal (Conico)"},
+    version =          "0.1",
+    dependencies =     { {id = "Avorion", min = "0.23"} },
+    serverSideOnly =   true,
+    clientSideOnly =   false,
     saveGameAltering = false,
-
-    -- Contact info for other users to reach you in case they have questions
-    contact = "arcturus615@gmail.com",
+    contact =          "@Cyberpunker#3611",
 }
