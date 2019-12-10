6de15ce174ffd1361dd41d436bff440e53e119eb
Andrew Wyatt <arcturus615@gmail.com>
2019-12-10 03:13:37 +0000

    isalivecron now checks against cron output size

--- root/usr/local/share/avorioncmd/cronjobs/isalivecron.sh
+++ root/usr/local/share/avorioncmd/cronjobs/isalivecron.sh
@@ -21,33 +21,23 @@ function main () {
 
 	local __tmpfile="$(mktemp)"
 	local __restarted=0
-	local __fintime
-	touch /tmp/avorion.hangdetector
+	local __fintime __cronoutput
 
+	touch /tmp/avorion.hangdetector
 	trap "rm -v /tmp/avorion.hangdetector ${__tmpfile} >/dev/null 2>>/srv/avorion/serverstatus.log" EXIT
 
 	echo >> /srv/avorion/serverstatus.log
 	echo "Log Started @ $__ETIME ($__DATE)" >> /srv/avorion/serverstatus.log
-	printf "Testing RCON..." >> /srv/avorion/serverstatus.log 
-	if ! timeout "$__TIMEOUT" $__RCON -c "$__RCONHOSTFILE" -s "$__GALAXY" status >>"$__tmpfile" 2>&1; then
+	printf "Testing RCON..." >> /srv/avorion/serverstatus.log
+
+	__cronoutput="$(timeout "$__TIMEOUT" $__RCON -c "$__RCONHOSTFILE" -s "$__GALAXY" status)"
+	if (( "$(wc -c "$__cronoutput")" < 200 )) 2>&1; then
 		echo "Failed to connect. Restarting Avorion." >>/srv/avorion/serverstatus.log
 		$__AVORION_CMD restart "${__GALAXY}" >>/srv/avorion/serverstatus.log 2>&1
 		sed 's,.*,\t&,' "$__tmpfile" >>/srv/avorion/serverstatus.log
 		__restarted=1
 	else
 		echo "Connected" >>/srv/avorion/serverstatus.log
-	fi
-
-	if (( __restarted < 1 )); then
-		__logfile="/srv/avorion/${__GALAXY}/serverlog-current.log"
-		printf "Checking logfile ${__logfile}..." >>/srv/avorion/serverstatus.log
-		if tail -n 10 "$__logfile" | grep -q '^Hang of at least 30 seconds detected' >/dev/null 2>&1; then
-			echo "Detected hang. Restarting Avorion" >> /srv/avorion/serverstatus.log
-			$__AVORION_CMD restart "${__GALAXY}" >> /srv/avorion/serverstatus.log
-			echo "" >"$__tmpfile"
-		else
-			echo "Passed check." >> /srv/avorion/serverstatus.log
-		fi
 	fi
 
 	cat "$__tmpfile" >> /srv/avorion/serverstatus.log
@@ -103,70 +93,6 @@ function __validate_setting_conf () {
 	done < /etc/avorionsettings.conf
 	
 	return 0
-}
-
-#@@@@@@@@@@@#
-# Utilities #
-#@@@@@@@@@@@#
-
-# int die <options> <string>
-#	Output the strings passed to stdout then exit
-#	with status code 1 (or the code given with -c)
-function die () {
-	local _code=1
-	if [[ "$1" == '-c' ]] && [[ "$2" =~ ^[0-9][0-9]*$ ]]; then
-		_code="$2"
-		shift; shift
-	fi
-
-	printf "Error: $1\n"
-	exit "$_code"
-}
-
-# bool yesno <string>
-#	Get a yes or no response from the user and
-#	return accordingly (0 for yes, 1 for no).
-#	If a string is passed, provide the user with
-#	that string as a prompt.
-function yesno () {
-	local _prompt _answer
-	_prompt="${1-Yes/No}"
-
-	while true; do
-		printf "[${_prompt}]> "
-		read -r _answer
-		case "${_answer}" in
-			[yY][eE][sS] | [yY] )
-				return 0
-				;;
-			[nN][oO] | [nN] )
-				return 1
-				;;
-			?)
-				printf "Please enter yes or no.\n"
-				;;
-		esac
-	done
-}
-
-# int plural <int>
-#	Given an int, print an s to the caller if said
-#	int does not equal 1
-function plural () {
-	if [[ "$1" =~ ^[0-9]+$ ]]; then
-		if (( $1 != 1 )); then
-			printf '%s' 's'
-			return 0
-		fi
-		return 0
-	fi
-
-	die "Function <plural> recieved <$1> rather than an int"
-}
-
-function getactive () {
-	local __unit_string='^[[:space:]]*avorion@[^[:space:]][^[:space:]]*.service[[:space:]][[:space:]]*loaded[[:space:]][[:space:]]*active[[:space:]][[:space:]]*running'
-	systemctl list-units 'avorion@*' | grep "$__unit_string" | awk '{print $1}' 2>/dev/null | sed 's,^avorion@,,; s,\.service$,,'
 }
 
 main "$@"
