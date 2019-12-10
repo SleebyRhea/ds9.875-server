fa2079e16071cf86d2d0dba454e1c08e41728228
Andrew Wyatt <arcturus615@gmail.com>
2019-12-10 15:53:41 +0000

    Hotfix welcomeemail for 0.29

--- root/srv/avorion/mods/ds9utils-welcomemail/data/scripts/galaxy/server.lua
+++ root/srv/avorion/mods/ds9utils-welcomemail/data/scripts/galaxy/server.lua
@@ -3,7 +3,7 @@ include("stringutility")
 include("weapontype")
 
 vanilla_onPlayerCreated = onPlayerCreated
-vanilla_onStartUp = onStartUp
+vanilla_initialize = initialize
 
 local turret = nil
 
@@ -12,17 +12,17 @@ local turret = nil
 --   helping me get this working and working out how to get
 --   good scaling for newbies!
 --]]
-function onStartUp()
-    vanilla_onStartUp()
+-- function initialize()
+--     vanilla_initialize()
 
-    --Sector x y and offset
-    local tx, ty, to = -200, -200, 275
+--     --Sector x y and offset
+--     local tx, ty, to = -200, -200, 275
 
-    -- Rarity, and Type
-    local tr, tt = 2, WeaponType.RawMiningLaser
+--     -- Rarity, and Type
+--     local tr, tt = 2, WeaponType.RawMiningLaser
 
-    turret = include("turretgenerator").generate(yx, ty, to, Rarity(tr), tt, Material(MaterialType.Naonite))
-end
+--     turret = include("turretgenerator").TurretGenerator.generateTurret(yx, ty, to, Rarity(tr), tt, Material(MaterialType.Naonite))
+-- end
 
 function onPlayerCreated (index)
     -- Call vanilla script
@@ -38,7 +38,7 @@ function onPlayerCreated (index)
     mail.sender = "DS9 Admin Team"
     mail.header = "Welcome to DS9.875!"
     mail.text = "Welcome to the DS9.875 Server! Here are some resources to get you going. If you have any questions, feel free to ask! Discord link: https://discord.gg/ZangShh"
-    mail:addTurret(turret)
+    -- mail:addTurret(turret)
     mail:setResources(iron,titanium,naonite,trinium,xanion,ogonite,avorion)
 
     player:addMail(mail)
