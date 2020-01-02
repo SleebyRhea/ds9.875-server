99c5b62123d2f97f2e749b33da72a2cfc8c76489
Andrew Wyatt <dreww7509@gmail.com>
2020-01-02 01:06:32 -0500

    ds9utils-lib can now set the modname on import

--- root/srv/avorion/mods/ds9utils-lib/data/scripts/lib/ds9utils-lib.lua
+++ root/srv/avorion/mods/ds9utils-lib/data/scripts/lib/ds9utils-lib.lua
@@ -1,9 +1,32 @@
 -- Print override for timestamps
 do
     local __vanilla_print = _G.print
+    local __d = {
+        i_modname = "ds9utils-lib",
+        i_loadermod = false
+    }
+
     function _G.print(...)
-        io.write("(" .. os.date("%Y-%m-%d %H:%M:%S") .. ") ")
+        local __modinfo = ""
+        local __timeinfo = "(" .. os.date("%Y-%m-%d %H:%M:%S") .. ") "
+
+        if __d.i_loadermod then
+            __modinfo = "[" .. __d.i_loadermod .. "] "
+        end
+
+        io.write(__timeinfo..__modinfo)
+
         return __vanilla_print(...)
     end
-end
 
+    return setmetatable(
+        {},
+        {
+            __call = function(_, modname)
+                if type(modname) == "string" then
+                    __d["i_loadermod"] = modname
+                end
+            end,
+        }
+    )
+end
