a12f06a98578e73f7578cd88765b18468675f493
Andrew Wyatt <dreww7509@gmail.com>
2020-01-02 00:00:05 -0500

    Use ISO formatting for date/time

--- root/srv/avorion/mods/ds9utils-lib/data/scripts/lib/ds9utils-lib.lua
+++ root/srv/avorion/mods/ds9utils-lib/data/scripts/lib/ds9utils-lib.lua
@@ -2,7 +2,7 @@
 do
     local __vanilla_print = _G.print
     function _G.print(...)
-        io.write("(" .. os.date("%H:%M:%S %m-%d-%Y") .. ")> ")
+        io.write("(" .. os.date("%Y-%m-%d %H:%M:%S") .. ") ")
         return __vanilla_print(...)
     end
 end
